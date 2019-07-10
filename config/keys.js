7418a68f890f51525a509bdefb21e281f0797f36
jaylamping <35737718+jaylamping@users.noreply.github.com>
2019-07-09 20:58:21 -0500

    server setup

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://jaylamping:Celtics2020$@test-cluster-hjaeu.mongodb.net/test?retryWrites=true&w=majority'
+};
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// initialize express
+const app = express();
+
+// body-parser middleware
+app.use(bodyParser.json());
+
+// assign db config
+const db = require('./config/keys').mongoURI;
+
+// connect to mongodb
+mongoose.connect(db, {useNewUrlParser: true})
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch(err => {
+    console.log(err);
+  });
+  const port = process.env.PORT || 5000;
+
+  app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+  });
