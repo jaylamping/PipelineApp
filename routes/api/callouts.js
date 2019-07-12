0d9d58bc80a4ed6e2cf7958e18ea0c63b60200b5
jaylamping <35737718+jaylamping@users.noreply.github.com>
2019-07-12 18:31:47 -0500

    react install

--- routes/api/callouts.js
+++ routes/api/callouts.js
@@ -7,30 +7,39 @@ const Callout = require('../../models/Callout');
 // @route GET api/callouts
 // @desc Get All Callouts
 // @access Public
-router.get('/', asyncHandler(async (req, res) => {
+router.get(
+  '/', 
+  asyncHandler(async (req, res) => {
     res.json(await Callout.find());
-}));
+  })
+);
 
 // @route POST api/callouts
 // @desc Post a callout
 // @access Public
-router.post('/', asyncHandler(async (req, res) => {
-  const newCallout = new Callout({
-    compressor: req.body.compressor,
-    explanation: req.body.explanation,
-    operator: req.body.operator,
-    alarmDtTm: req.body.alarmDtTm,
-    addNotes: req.body.addNotes
-  });
-  res.json(await newCallout.save())
-}));
+router.post(
+  '/', 
+  asyncHandler(async (req, res) => {
+    const newCallout = new Callout({
+      compressor: req.body.compressor,
+      explanation: req.body.explanation,
+      operator: req.body.operator,
+      alarmDtTm: req.body.alarmDtTm,
+      addNotes: req.body.addNotes
+    });
+  res.json(await newCallout.save());
+  })
+);
 
 // @route DELETE api/callouts/:id
 // @desc Delete a callout
 // @access Public
-router.delete('/:id', asyncHandler(async (req, res) => {
-  let document = await Callout.findById(req.params.id);
-  res.json(await document.remove())
-}));
+router.delete(
+  '/:id', 
+  asyncHandler(async (req, res) => {
+    let document = await Callout.findById(req.params.id);
+    res.json(await document.remove())
+  })
+);
 
 module.exports = router;
