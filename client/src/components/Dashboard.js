49e8b7dc7b70e745341f8a9f1a82ce5d04116165
jaylamping <35737718+jaylamping@users.noreply.github.com>
2019-07-15 21:19:32 -0500

    dashboard and landing logic done

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Container } from 'reactstrap';
+import { Redirect } from 'react-router-dom';
 import OpenCallouts from './OpenCallouts';
 import CalloutModal from './CalloutModal';
+import PropTypes from 'prop-types';
 
 class Dashboard extends Component {
   
+  static propTypes = {
+    isAuthenticated: PropTypes.bool
+  };
+
   render() {
+    // redirect to landing if not authenticated
+    if(!this.props.isAuthenticated) {
+      return <Redirect to='/' />
+    };
+
     return(
       <Container>
         <CalloutModal />
@@ -18,7 +29,7 @@ class Dashboard extends Component {
 };
 
 const mapStateToProps = state => ({
-
+  isAuthenticated: state.auth.isAuthenticated
 });
 
 export default connect(
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Landing extends Component {
-  
-  render() {
-    return(
-      <div>
-        hello
-      </div>
-    )
+
+  static propTypes = {
+    isAuthenticated: PropTypes.bool
   };
 
+  render() {
+    // redirect to dashboard if authenticated
+    if(this.props.isAuthenticated) {
+      return <Redirect to='/dashboard' />
+    };
+
+    return(
+      <section className='landing'>
+        <div className='overlay'>
+          <div className='landing-inner'>
+            <h1 className='x-large'>Pipeline App</h1>
+          </div>
+        </div>
+      </section>
+    )
+  };
 };
 
-const mapStateToProps = state => ({
 
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
 });
+
 
 export default connect(
   mapStateToProps,
